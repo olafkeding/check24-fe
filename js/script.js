c27969091b490291f47c9a909fe6f1c4df354689
Olaf Keding <olaf.keding@check24.de>
2019-07-17 10:33:13 +0200

    webcomp

--- js/script.js
+++ js/script.js
@@ -13,12 +13,17 @@
         }
     }
 
-    //TODO
-    //- change function so that it receives a parameter object as second argument {paramKey1 : "param1Value", paramKey2 : "param3Value"}
+    //TODO #2
+    //- the old Plz-Api has ben deprecated (oh no!)
+    //- this is the new api, it only supports POST : http://schulung.bloffen.de/ajax/plz_lookup.2.php
+    //- change  the function below, so that it receives a parameter object as second argument {paramName1 : "param1Value", paramName2 : "param3Value"}
     //- make the get function accessible through a wrapper function "ajax" ajax(url).get(data, callback)
     //- add a function ajax(url).post(data, callback) that uses "POST" instead of "GET" as method
-    //  - don't duplicate code
-    //  - make sure the common function is private (not accessible through ajax().)
+    //   - use the header 'Content-Type': 'application/x-www-form-urlencoded' (request.setRequestHeader)
+    //   - POST parameters are passes as a query string in the request body (request.send(paramsString))
+    //- use that function int the plz lookup
+    //- don't duplicate code
+    //- make sure the common function is private (not accessible through ajax().)
 
     function ajax(url) {
         var POST = "POST";
@@ -28,11 +33,9 @@
             var request = new XMLHttpRequest();
             var paramsString = "";
 
-            for(var key in data){
-                if(data.hasOwnProperty(key)) {
-                    paramsString += key + "=" + data[key] + "&";
-                }
-            }
+            Object.keys(data).forEach(function(key) {
+                paramsString += key + "=" + data[key] + "&";
+            });
 
             if(method === GET) {
                 url += "?" + paramsString;
@@ -43,14 +46,19 @@
             request.onload = function() {
                 if (request.status >= 200 && request.status < 400) {
                     // Success!
+                    var parsedData;
                     try {
-                        callback({status: "SUCCESS", data : JSON.parse(request.responseText)});
+                        parsedData = JSON.parse(request.responseText);
+
                     } catch (error) {
-                        callback({status: "ERROR", message : "cannot parse response: " + error.message});
+                        callback({status: "ERROR", message: "cannot parse response"});
                     }
+                    callback({status: "SUCCESS", data: parsedData});
+
                 } else {
                     // We reached our target server, but it returned an error
-                    callback({status: "ERROR", message : "no data"});
+                    callback({status: "ERROR", message: "no data"});
+
                 }
             };
 
@@ -77,12 +85,16 @@
     documentReady(function(){
         var plzElement = document.getElementById("plz");
         plzElement.addEventListener("input", function(event) {
-            //TODO
-            //- use the get function (or ajax().get) to load the ort options form the server. The API supports two plz: "81539" and "16845"
+            //TODO #1
+            //- use the get function to load the ort options form the server
+            //- Url: http://schulung.bloffen.de/ajax/plz_lookup.php?plz={PLZ_VALUE}
+            //- the API supports two plz: "81539" and "16845"
             //- only dispatch the call if the user entered 5 digits
-            //- if the server does not return a plz, show an error
+            //- if the server returns an empty list, show an error in the element with the class "error"
             //- if the server does return one option, display it in a span with the class "ort-label"
-            //- if the server returns more than one option, display then in the select element with the name "ort"
+            //- if the server returns more than one option, display then in the select element with the name "city"
+            //- sync the value of the city into the hidden input field with the ID city
+            //- if both zipcode and city have a valid value, show th button <button type="submit">SEND</button>
             var val = this.value;
             if(val.length === 5) {
                 ajax("/plz_lookup.php").get({plz: val}, function(response){
